e34fa5a17eba90ad2643503a075dc4b48b769708
Ghabi Anis <73026191+ghabianis@users.noreply.github.com>
2026-01-06 14:50:49 +0100

    update resume link
    Updated address and resume link in personal data.

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Anis Ghabi. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'anisghabi8@gmail.com',
   phone: '+21690770350',
-  address: 'Tunisia, Sousse shaloul 4000',
+  address: 'Tunisia, Tunis',
   github: 'https://github.com/ghabianis',
   facebook: 'https://www.facebook.com/ghabi.anis.3',
   linkedIn: 'https://www.linkedin.com/in/ghabi-anis/',
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/15451730/ghabi-anis',
   leetcode: "https://leetcode.com/ghabianis/",
   devUsername: "ghabianis",
-  resume: "https://drive.google.com/file/d/1mGXSEcwOmWLWjvMFVF00i2W5G-z4HdlP/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1w9DzN8_8SWfM7QMwJqImMgBFy9KJnXev/view?usp=sharing"
 }
